03598074a0cd49389d88a6d9d79bed32bb95d64d
Astrida Jablonskytė <astrida789@gmail.com>
2025-05-23 23:03:31 +0300

    first comment

---
+++ main.cpp
@@ -0,0 +1,10 @@
+
+int main(){
+    /*
+    naudoti maps ir imt zodi kaip key ir tada pair kai vektoriu ir i ji deti visas eilutes kuriose kartojasi zodis ir tada
+     jo lenght paimt pirmai uzduociai o antrai uzduociai galiu paimt ir tsg coutint 
+    vektoriu i rten bus visos eilutes kuriose yra tas zodis bbz 
+    
+    
+    */
+}
